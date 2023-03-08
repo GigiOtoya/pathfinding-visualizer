19cd1b4635528cf456b70581e3d76e700cbb9869
Luigi Otoya <Luigi.Otoya@gmail.com>
2023-03-08 15:17:34 -0500

    basic styling

---
+++ dijkstra/code.js
@@ -0,0 +1,22 @@
+const canvas = document.getElementById("canvas");
+
+function drawCanvas() {
+    const ctx = canvas.getContext("2d");
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.clientWidth, canvas.height);
+}
+
+function drawNode() {
+    const canvas = document.getElementById("canvas");
+    if (canvas.getContext) {
+        const ctx = canvas.getContext("2d");
+
+        ctx.fillStyle = "rgb(200, 0, 0)";
+        ctx.fillRect(10, 10, 50, 50);
+
+        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+        ctx.fillRect(30, 30, 50, 50);
+    }
+}
+
+drawCanvas();
