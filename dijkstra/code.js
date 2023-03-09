d6660d9b952502ef15477dd10d58f308a6018ea5
Luigi Otoya <Luigi.Otoya@gmail.com>
2023-03-09 14:41:33 -0500

    node class added

--- dijkstra/code.js
+++ dijkstra/code.js
@@ -3,21 +3,43 @@ const ctx = canvas.getContext("2d");
 canvas.width = 1280;
 canvas.height = 720;
 
+let visited = [];
+let unvisited = [];
+
+nodeBtn = document.getElementById("add-node-btn");
+edgeBtn = document.getElementById("add-edge-btn");
+
+// Event Listeners
+nodeBtn.addEventListener("click", addNode);
+
 
 function drawCanvas() {
     ctx.fillStyle = "#1b1b1b";
     ctx.fillRect(0, 0, canvas.clientWidth, canvas.height);
 }
 
-function drawNode() {
-    ctx.beginPath();
-    ctx.arc(canvas.width / 2, canvas.height / 2, 50, 0, Math.PI * 2, false);
-    ctx.fillStyle = "lightsteelblue";
-    ctx.strokeStyle = "white";
-    ctx.lineWidth = 3;
-    ctx.fill();
-    ctx.stroke();
-}
+// Circle constructor
+function Node(x, y, r) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    
+    this.drawNode = function() {
+        ctx.beginPath();
+        ctx.arc(x, y, r, 0, Math.PI *2);
+        ctx.fillStyle = "lightsteelBlue";
+        ctx.strokeStyle = "white";
+        ctx.lineWidth = 3;
+        ctx.fill();
+        ctx.stroke();
+    };
+};
+
+// Button functions
+function addNode(e) {
+    e.preventDefault();
+    let node = new Node(canvas.width / 2, canvas.height / 2, 50);
+    node.drawNode();
+};
 
 drawCanvas();
-drawNode();
