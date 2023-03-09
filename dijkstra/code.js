6f950076b9854b3e39226090c983403c6eac4660
Luigi Otoya <Luigi.Otoya@gmail.com>
2023-03-09 13:02:03 -0500

    draw node added

--- dijkstra/code.js
+++ dijkstra/code.js
@@ -1,25 +1,23 @@
 const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 canvas.width = 1280;
 canvas.height = 720;
 
 
 function drawCanvas() {
-    const ctx = canvas.getContext("2d");
-    ctx.fillStyle = "black";
+    ctx.fillStyle = "#1b1b1b";
     ctx.fillRect(0, 0, canvas.clientWidth, canvas.height);
 }
 
 function drawNode() {
-    const canvas = document.getElementById("canvas");
-    if (canvas.getContext) {
-        const ctx = canvas.getContext("2d");
-
-        ctx.fillStyle = "rgb(200, 0, 0)";
-        ctx.fillRect(10, 10, 50, 50);
-
-        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-        ctx.fillRect(30, 30, 50, 50);
-    }
+    ctx.beginPath();
+    ctx.arc(canvas.width / 2, canvas.height / 2, 50, 0, Math.PI * 2, false);
+    ctx.fillStyle = "lightsteelblue";
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 3;
+    ctx.fill();
+    ctx.stroke();
 }
 
 drawCanvas();
+drawNode();
